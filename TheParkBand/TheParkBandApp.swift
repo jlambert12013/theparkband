f2ab435059f7024f832e5d7b979857968a8c668d
Jim Lambert <jimlambert@Jims-Mac-mini.attlocal.net>
2025-02-05 01:09:21 -0600

    Initial Commit

---
+++ TheParkBand/TheParkBandApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheParkBandApp.swift
+//  TheParkBand
+//
+//  Created by Jim Lambert on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TheParkBandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
