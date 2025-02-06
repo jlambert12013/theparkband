5edea6fcb339bbf36627c614e8729cf9970986c7
Jim Lambert <jimlambert@Jims-Mac-mini.attlocal.net>
2025-02-06 16:31:03 -0600

    ADDED FLOATING TAB BAR WITH PLACEHOLDER EXAMPLE

--- TheParkBand/MainView.swift
+++ TheParkBand/MainView.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import FloatingTabBar
 
 struct MainView: View {
   @EnvironmentObject var authenticationViewModel: AuthenticationViewModel
+  @State private var selectedIndex: Int = 0
+  
+  @State var items: [BottomBarItem] = [
+    BottomBarItem(icon: "house.fill", color: .red),
+    BottomBarItem(icon: "magnifyingglass", color: .red),
+    BottomBarItem(icon: "heart", color: .red),
+    BottomBarItem(icon: "person.fill", color: .red),
+  ]
+
+//  let items: [BottomBarItem] = [
+//    BottomBarItem(icon: "house", color: Color.red)
+//  ]
+//
+//  var selectedItem: BottomBarItem {
+//      items[selectedIndex]
+//  }
   var body: some View {
     VStack {
       Text("MAIN VIEW")
@@ -17,7 +34,12 @@ struct MainView: View {
           try authenticationViewModel.signOut()
         }
       }
+      
+      BottomBar(selectedIndex: $selectedIndex, items: $items)
+   
     }
+    
+    
   }
 }
 
